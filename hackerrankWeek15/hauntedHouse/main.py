b7a5387ae26e3cbdf07eb4caac09facc2426fe8a
James <jameswo@berkeley.edu>
2015-07-03 23:42:47 -0700

    bug fix hh

--- hackerrankWeek15/hauntedHouse/main.py
+++ hackerrankWeek15/hauntedHouse/main.py
@@ -1,30 +1,32 @@
 # Hackerrank week15 2015
 # haunted house
+# https://www.hackerrank.com/contests/w15/challenges/haunted-house
 
 def solve(arr, n):
-    # returns the maximum number of intervals that can be satisfied at once
-    startPoints = { x:0 for x in range(n) }
-    endPoints = { x:0 for x in range(n) }
-    
+    maxnstart = max([a[0] for a in arr]+[n])
+    maxnend = max([a[1] for a in arr]+[n])
+    startPoints = { x:0 for x in range(maxnstart+1) }
+    endPoints = { x:0 for x in range(maxnend+1) }
     count = 0
-    # count the number of intervals that are satisfied with n=0
     for start, end in arr:
-	startPoints[start]+=1
-	endPoints[end]+=1
-	if start <= 0:
-	    count += 1
-
-    best = count
+            startPoints[start]+=1
+            endPoints[end]+=1
+            if start <= 0:
+                count += 1
+    best = 1 if count > 0 else 0
     lastSatisfied = count
-    for num in range(1, n):
-	# the number of intervals satisfied with k people is the
-	# number of intervals satisfied with k-1 people 
-	# plus the number of intervals starting on k
-	# minus the number of intervals that ended on k-1
-	satisfied = lastSatisfied + startPoints[num] - endPoints[n-1]
-	lastSatisfied = satisfied
-	best = max(best, satisfied)
-
+    for num in range(1, n+1):
+        satisfied = lastSatisfied + startPoints[num] - endPoints[num-1]
+        lastSatisfied = satisfied
+        if satisfied >= num:
+            best = num
     return best
 
-
+n = input()
+ranges = []
+for _ in range(n):
+    x, y = map(int, raw_input().split())
+    x += 1
+    y += 1
+    ranges.append((x,y))
+print solve(ranges, n)
