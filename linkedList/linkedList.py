660106ccb9e0844e77cbd5adb643dd40261632ac
James <jameswo@berkeley.edu>
2015-07-04 19:52:39 -0700

    LL

--- linkedList/linkedList.py
+++ linkedList/linkedList.py
@@ -1,12 +1,46 @@
 # singly linked list
 
+# super simple node object.
 class node(object):
     value = None
     next = None
-    def __init__(self, value=None, next=None):
+    def __init__(self, value, next=None):
         self.value = value
         self.next = next
+	
+    def __str__(self):
+        retStr = []
+        ptr = self
+        while ptr:
+            retStr.append(ptr.value)
+            ptr = ptr.next
+        return ", ".join(map(str,retStr))
+    
+# create a linkedList given a python list l
+# returns head node
+def createLinkedList(l):
+    if not l:
+        return None
+    head = None
+    for i in reversed(l):
+        head = node(i, head)
+    return head
 
+def testNode():
+    n = node(3)
+    assert n.__str__() == '3'
+    n = node(4, n)
+    assert n.__str__() == '4, 3'
+    # test createLinkedList
+    assert createLinkedList([]) == None
+    assert createLinkedList([1]).__str__() == '1'
+    assert createLinkedList([1,3,2]).__str__() == '1, 3, 2'
+
+    print 'pass'
+
+# a wrapper for a linkedList object
+# most of the time, this will not be used, and we will
+# just say a node object representing the head is the LL
 class linkedList(object):
     head = None
     length = 0
