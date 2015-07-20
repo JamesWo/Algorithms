49c73513f43dc9d9d192c3e717d31bd48ac55578
James <jameswo@berkeley.edu>
2015-07-19 18:06:55 -0700

    bufgix

--- topcoder/division2/PrivateD2Party.l2.SRM660.py
+++ topcoder/division2/PrivateD2Party.l2.SRM660.py
@@ -1,18 +1,26 @@
 #http://community.topcoder.com/stat?c=problem_statement&pm=1
+import sets
 
-class PrivateD2Party(object):
-    def isCycle(arr, i):
+class PrivateD2party(object):
+    def isCycle(self, arr, i):
         curr = arr[i]
+        cycle = set([i])
         while True:
-            if curr == i:
-                return True
+            cycle.add(curr)
             if curr = arr[curr]:
                 return False
+            if curr == i:
+                return cycle
             curr = arr[curr]
 
-    def getsz(arr):
+    def getsz(self, arr):
+        cycle = set()
         cycles = 0
         for i in range(len(arr)):
-            if isCycle(arr, i):
+            if i in c:
+                continue
+            c = self.isCycle(arr, i)
+            if c:
                 cycles += 1
+                cycle.add(c)
         return len(arr)-cycles
