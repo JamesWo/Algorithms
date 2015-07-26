abac460c76f060ff54f5750c6009e2af276fb3b1
James <jameswo@berkeley.edu>
2015-07-26 12:36:13 -0700

    getNumWays.py

--- dp/getNumWays.py
+++ dp/getNumWays.py
@@ -3,10 +3,10 @@ def getNumWays(n):
         raise Exception
     if n < 0:
         raise Exception
-    numWays = [0, 1, 2]
+    numWays = [1, 1, 2]
     for i in range(3, n+1):
         numWays.append(
-                numWays[i-1] + 2*numWays[i-2] + 4*numWays[i-3] 
+                numWays[i-1] + numWays[i-2] + numWays[i-3] 
                 )
     return numWays[n]
 
@@ -15,8 +15,12 @@ def getNumWays(n):
 
 cases = {
         3:4,
-        0:0,
+        0:1,
+        4:7,
+        5:13,
+        6:24
         }
 
 for inp, out in cases.iteritems():
-    assert getNumWays(inp) == out
+    assert getNumWays(inp) == out, "inp: %s,\nout: %s\nexpected:%s" % (
+            inp, getNumWays(inp), out )
