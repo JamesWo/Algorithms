97370940239b29e1c9cfe82b99395954a22a705f
James <jameswo@berkeley.edu>
2015-10-15 23:01:27 -0700

    divide two integers

---
+++ leet/divideTwoIntegers.py
@@ -0,0 +1,26 @@
+"""
+Divide two integers without using multiplication,
+division, or mod operator.
+"""
+
+class Solution:
+    def divide(dividend, divisor):
+        assert isinstance(dividend, int)
+        assert isinstance(divisor, int)
+        if divisor == 0:
+            raise Exception
+
+
+
+
+#        binaryList = [divisor]
+#        multipleOfDivisor = 1
+#        totalDivisor = divisor
+#        while dividend > totalDivisor + totalDivisor:
+#            totalDivisor = totalDivisor + totalDivisor
+#            multipleOfDivisor = multipleOfDivisor + multipleOfDivisor
+#            binaryList.append(totalDivisor)
+#        total = totalDivisor
+#
+
+
