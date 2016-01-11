1952e0f7479844e21ea94e189d08f3700153b2a6
James <jameswo@berkeley.edu>
2016-01-10 23:39:07 -0800

    update inversions algo

--- hackerrank/hourRank4/newYearChaos.py
+++ hackerrank/hourRank4/newYearChaos.py
@@ -1,9 +1,53 @@
 #!/bin/python
 
-# https://www.hackerrank.com/contests/hourrank-4/challenges/new-year-chaos
-
 import sys
 
+def countInversions(lst):
+    # divide and conquer algorithm for counting the number of
+    # pairs of elements in the list which are inverted
+    # This function modifies original list.
+    # see http://www.geeksforgeeks.org/counting-inversions/
+    return _countInversions(lst, 0, len(lst)-1)
+
+def _countInversions(lst, left, right):
+    if left >= right:
+        return 0
+    mid = (left + right) / 2
+    count = 0
+    count += _countInversions(lst, left, mid)
+    count += _countInversions(lst, mid+1, right)
+    count += _merge(lst, left, mid+1, right)
+    return count
+
+def _merge(lst, left, mid, right):
+    count = 0
+    leftIndex = left
+    rightIndex = mid
+    tmp = []
+    while ((leftIndex <= mid-1) and (rightIndex <= right)):
+        if lst[leftIndex] <= lst[rightIndex]:
+            tmp.append(lst[leftIndex])
+            leftIndex += 1
+        else:
+            tmp.append(lst[rightIndex])
+            rightIndex += 1
+            # the number of inversions is incremented by the number
+            # of remaining elements in the left sublist
+            count += (mid-leftIndex)
+    tmp.extend(lst[leftIndex:mid])
+    tmp.extend(lst[rightIndex:right+1])
+    lst[left:right+1] = tmp
+    return count
+        
+def countInversions2(lst):
+    # O(N^2) algorithm for counting inversions, for testing purposes
+    count = 0
+    for i in range(size):
+        for j in range(i+1, size):
+            if lst[i] > lst[j]:
+                count += 1
+    return count
+   
 def solve(size, queue):
     # if any member of queue is more than 2 positions ahead of 
     # their original location, then the state is impossible
@@ -11,13 +55,8 @@ def solve(size, queue):
         if queue[i] >= i+4:
             return "Too chaotic"
     # otherwise count the number of mismatched pairs - in nlogn time
-    # see http://www.geeksforgeeks.org/counting-inversions/
-    count = 0
-    for i in range(size):
-        for j in range(i+1, size):
-            if queue[i] > queue[j]:
-                count += 1
-    return count
+    return countInversions(queue)    
+
     
 numCases = int(raw_input().strip())
 for a0 in xrange(numCases):
@@ -25,4 +64,7 @@ for a0 in xrange(numCases):
     queue = map(int,raw_input().strip().split(' '))
     # your code goes here
     print solve(size, queue)
+    
+
+
 
